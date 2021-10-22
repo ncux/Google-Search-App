317a58e2be3fdedcd0d80b57f5cd475244839c0d
ncux <malabatalent@gmail.com>
2021-10-22 16:27:01 +0800

    fixed 403 API issue

--- pages/search.js
+++ pages/search.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import Results from "../components/results/results";
+import Layout from "../components/layout/layout";
 
 export default function SearchPage() {
 
     return (
-        <div >
-            <Results />
-        </div>
+        <Layout title="Google | Search" >
+            <div className="dark:bg-gray-900 dark:text-gray-200">
+                <Results />
+            </div>
+        </Layout>
     );
 }
